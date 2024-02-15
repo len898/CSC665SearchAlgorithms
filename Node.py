276c35122960e00a112e83f6ed1eb3931a28b394
amogh2004 <amoghpavan5363@gmail.com>
2024-02-15 12:21:28 -0800

    Adding Nodes

---
+++ Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, city_name):
+        self.city_name = city_name
+        self.neighbors = []  # Represents the neighboring cities
+
+    def add_neighbor(self, neighbor_city):
+        self.neighbors.append(neighbor_city)
+
+    def __repr__(self):
+        return f"Node(city_name={self.city_name})"
--- main.py
+++ main.py
@@ -1,8 +1,102 @@
 # Task 1
+# Node.py file has been added
 
-#Task 2
+# Task 2
+from Node import Node
 
-#Task 3
+# Creating nodes for each city
+arad = Node("Arad")
+bucharest = Node("Bucharest")
+craiova = Node("Craiova")
+drobeta = Node("Drobeta")
+eforie = Node("Eforie")
+fagaras = Node("Fagaras")
+giurgiu = Node("Giurgiu")
+hirsova = Node("Hirsova")
+iasi = Node("Iasi")
+lugoj = Node("Lugoj")
+mehadia = Node("Mehadia")
+neamt = Node("Neamt")
+oradea = Node("Oradea")
+pitesti = Node("Pitesti")
+rimnicu_vilcea = Node("Rimnicu Vilcea")
+sibiu = Node("Sibiu")
+timisoara = Node("Timisoara")
+urziceni = Node("Urziceni")
+vaslui = Node("Vaslui")
+zerind = Node("Zerind")
+
+# Adding neighbors for each city (assuming an undirected graph)
+arad.add_neighbor(sibiu)
+arad.add_neighbor(timisoara)
+arad.add_neighbor(zerind)
+
+bucharest.add_neighbor(fagaras)
+bucharest.add_neighbor(giurgiu)
+bucharest.add_neighbor(pitesti)
+bucharest.add_neighbor(urziceni)
+
+craiova.add_neighbor(drobeta)
+craiova.add_neighbor(pitesti)
+craiova.add_neighbor(rimnicu_vilcea)
+
+drobeta.add_neighbor(craiova)
+drobeta.add_neighbor(mehadia)
+
+eforie.add_neighbor(hirsova)
+
+fagaras.add_neighbor(bucharest)
+fagaras.add_neighbor(sibiu)
+
+giurgiu.add_neighbor(bucharest)
+
+hirsova.add_neighbor(eforie)
+hirsova.add_neighbor(urziceni)
+
+iasi.add_neighbor(neamt)
+iasi.add_neighbor(vaslui)
+
+lugoj.add_neighbor(mehadia)
+lugoj.add_neighbor(timisoara)
+
+mehadia.add_neighbor(drobeta)
+mehadia.add_neighbor(lugoj)
+
+neamt.add_neighbor(iasi)
+
+oradea.add_neighbor(zerind)
+
+pitesti.add_neighbor(bucharest)
+pitesti.add_neighbor(craiova)
+pitesti.add_neighbor(rimnicu_vilcea)
+
+rimnicu_vilcea.add_neighbor(craiova)
+rimnicu_vilcea.add_neighbor(pitesti)
+rimnicu_vilcea.add_neighbor(sibiu)
+
+sibiu.add_neighbor(arad)
+sibiu.add_neighbor(fagaras)
+sibiu.add_neighbor(rimnicu_vilcea)
+
+timisoara.add_neighbor(arad)
+timisoara.add_neighbor(lugoj)
+
+urziceni.add_neighbor(bucharest)
+urziceni.add_neighbor(hirsova)
+urziceni.add_neighbor(vaslui)
+
+vaslui.add_neighbor(iasi)
+vaslui.add_neighbor(urziceni)
+
+zerind.add_neighbor(arad)
+zerind.add_neighbor(oradea)
+
+# Storing all the nodes in a list for easy access
+cities = [arad, bucharest, craiova, drobeta, eforie, fagaras, giurgiu, hirsova, iasi, lugoj,
+          mehadia, neamt, oradea, pitesti, rimnicu_vilcea, sibiu, timisoara, urziceni, vaslui, zerind]
+
+
+# Task 3
 import queue
 
 fifo_queue = queue.Queue()
@@ -12,8 +106,8 @@ priority_queue = queue.PriorityQueue()
 visited_1 = {}
 visited_2 = {}
 visited_3 = {}
-#Task 4
+# Task 4
 
-#Task 5
+# Task 5
 
-#Task 6
+# Task 6
