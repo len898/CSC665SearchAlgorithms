e60265fc7caea6e1fc57a771417bbd742fb77c61
lennart@familie-richter.de <lennart@familie-richter.de>
2024-02-15 10:06:32 -0800

    Queues and Visited

--- main.py
+++ main.py
@@ -3,7 +3,15 @@
 #Task 2
 
 #Task 3
+import queue
 
+fifo_queue = queue.Queue()
+lifo_queue = queue.LifoQueue()
+priority_queue = queue.PriorityQueue()
+
+visited_1 = {}
+visited_2 = {}
+visited_3 = {}
 #Task 4
 
 #Task 5
